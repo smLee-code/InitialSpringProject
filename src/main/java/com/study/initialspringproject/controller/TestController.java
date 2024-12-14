3a75e8e982f9015212becc1c2bf422edd0688885
Seungyeon Kang <tmddusgood@gmail.com>
2024-12-14 21:19:02 +0900

    Revert "fifeat: 수정사항 요ì²컨컨트롤러 반영"
    This reverts commit 2921abb34eb2e308d402869363aa2ed9bd3c2506.

--- src/main/java/com/study/initialspringproject/controller/TestController.java
+++ src/main/java/com/study/initialspringproject/controller/TestController.java
@@ -1,7 +1,5 @@
 package com.study.initialspringproject.controller;
 
-import com.study.initialspringproject.service.TestService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,10 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@RequiredArgsConstructor
 public class TestController {
-
-    private final TestService testService;
 
     @GetMapping("/hello")
     public String test() {
@@ -21,6 +16,6 @@ public class TestController {
 
     @GetMapping("/parameter")
     public String getTestParameter(@RequestParam String name) {
-        return testService.testMethod(name);
+        return name;
     }
 }
