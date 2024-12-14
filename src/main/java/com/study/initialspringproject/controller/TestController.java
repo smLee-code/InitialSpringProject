c80edaf45a28347869a4f505fdd790e6fa997f81
Sang-Myeong Lee <teamjuji5@naver.com>
2024-12-14 20:54:24 +0900

    feat: ì¶@RequestParam 사용하는 테스트 기능 추가

--- src/main/java/com/study/initialspringproject/controller/TestController.java
+++ src/main/java/com/study/initialspringproject/controller/TestController.java
@@ -2,6 +2,7 @@ package com.study.initialspringproject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class TestController {
     @GetMapping("/hello")
     public String test() {
         return "Hello World";
+    }
+
+    @GetMapping("/test")
+    public String getTestParameter(@RequestParam String name) {
+        return name;
     }
 }
