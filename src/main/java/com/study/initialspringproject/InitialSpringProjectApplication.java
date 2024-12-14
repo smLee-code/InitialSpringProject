a5509f01bc28687dd6c0757d0f6b1c485d1248a0
Sang-Myeong Lee <teamjuji5@naver.com>
2024-12-14 20:39:08 +0900

    Initial commit

---
+++ src/main/java/com/study/initialspringproject/InitialSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.study.initialspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitialSpringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/initialspringproject/InitialSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.initialspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
