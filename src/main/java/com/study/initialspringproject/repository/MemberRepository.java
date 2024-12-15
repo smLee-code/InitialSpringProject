1b2c98626cdb206e2826d0457e6f3538cc1e0252
Sang-Myeong Lee <teamjuji5@naver.com>
2024-12-16 00:10:29 +0900

    Update Projcet: Component Scan Annotation Exist

--- src/main/java/com/study/initialspringproject/repository/MemberRepository.java
+++ src/main/java/com/study/initialspringproject/repository/MemberRepository.java
@@ -1,4 +1,15 @@
 package com.study.initialspringproject.repository;
 
+import com.study.initialspringproject.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+    
 }
--- src/test/java/com/study/initialspringproject/service/MemberServiceTest.java
+++ src/test/java/com/study/initialspringproject/service/MemberServiceTest.java
@@ -1,4 +1,78 @@
+package com.study.initialspringproject.service;
+
+import com.study.initialspringproject.domain.Member;
+import com.study.initialspringproject.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class MemberServiceTest {
-  
+
+    MemberService memberService;
+    MemoryMemberRepository memoryRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memoryRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memoryRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memoryRepository.clearStoreAndResetSequence();
+    }
+
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("spring");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+/*
+        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+*/
+        // then
+
+    }
+
+    @Test
+    void 전체_회원_조회() {
+    }
+
+    @Test
+    void 단일_회원_조회() {
+    }
 }
