57a8ff37697b51c2dbc9e2b9ae7aed21140f9e13
Sang-Myeong Lee <teamjuji5@naver.com>
2024-12-16 00:27:00 +0900

    Update Project: DI by Java Code Directly

---
+++ src/main/java/com/study/initialspringproject/service/SpringConfig.java
@@ -0,0 +1,20 @@
+package com.study.initialspringproject.service;
+
+import com.study.initialspringproject.repository.MemberRepository;
+import com.study.initialspringproject.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
