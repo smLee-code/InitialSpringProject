af981e3e867790f442ea320f3bb4656436b32f82
Sang-Myeong Lee <teamjuji5@naver.com>
2024-12-16 16:32:53 +0900

    Update JDBC-only

--- src/main/java/com/study/initialspringproject/repository/JdbcMemberRepository.java
+++ src/main/java/com/study/initialspringproject/repository/JdbcMemberRepository.java
@@ -19,16 +19,19 @@ public class JdbcMemberRepository implements MemberRepository {
 
     @Override
     public Member save(Member member) {
+
         String sql = "insert into member(name) values(?)";
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
+
         try {
             conn = getConnection();
             pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             pstmt.setString(1, member.getName());
             pstmt.executeUpdate();
             rs = pstmt.getGeneratedKeys();
+
             if (rs.next()) {
                 member.setId(rs.getLong(1));
             } else {
