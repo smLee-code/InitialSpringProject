64d86a20182f22a5fdc8b458e9476f408f97568b
Sang-Myeong Lee <teamjuji5@naver.com>
2024-12-16 20:55:19 +0900

    SpringData JPA

--- src/main/java/com/study/initialspringproject/repository/MemberRepository.java
+++ src/main/java/com/study/initialspringproject/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package com.study.initialspringproject.repository;
 
 import com.study.initialspringproject.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
